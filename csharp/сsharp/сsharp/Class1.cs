1f643cd3c370899980d10e10107612b75e6caf7a
Xxy8gxiydy8d8ydy8dxyixtixtixoyxy8x8ycyociycidyic8yxitxtixiyxyxyixyix <9696650@stud.kai.edu.ua>
2026-02-19 08:56:04 +0200

    Update Class1.cs

--- csharp/сsharp/сsharp/Class1.cs
+++ csharp/сsharp/сsharp/Class1.cs
@@ -12,34 +12,34 @@ namespace сsharp
     {
         public class StringHandler
         {
-            // Приватне поле для зберігання рядка
+        
             private string _data;
 
-            // Конструктор класу
+            
             public StringHandler(string input)
             {
                 _data = input;
             }
 
-            // Метод для повернення значення рядка (у великих буквах)
+            
             public string GetUpperCaseValue()
             {
                 return _data.ToUpper();
             }
 
-            // Метод для обчислення довжини рядка
+            
             public int GetLength()
             {
                 return _data.Length;
             }
 
-            // Метод для заміни одного символу на інший
+           
             public void ReplaceChar(char oldChar, char newChar)
             {
                 _data = _data.Replace(oldChar, newChar);
             }
 
-            // Метод для отримання поточного стану рядка
+            
             public string GetCurrentValue()
             {
                 return _data;
