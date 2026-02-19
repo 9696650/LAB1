ff4c165769b3633e3a84c84006260d48d1fb8bd9
Xxy8gxiydy8d8ydy8dxyixtixtixoyxy8x8ycyociycidyic8yxitxtixiyxyxyixyix <9696650@stud.kai.edu.ua>
2026-02-19 08:57:44 +0200

    Update Program.cs

--- csharp/сsharp/сsharp/Program.cs
+++ csharp/сsharp/сsharp/Program.cs
@@ -13,28 +13,24 @@ namespace сsharp
         {
             static void Main(string[] args)
             {
-                // 1. Введення даних користувачем
+            
                 Console.Write("Write a word: ");
                 string input = Console.ReadLine() ?? "";
 
-                // 2. Створення об'єкта класу (екземпляра)
+                /
                 StringHandler handler = new StringHandler(input);
 
 
-                // Вивід рядка великими літерами
                 Console.WriteLine($"CAPS: {handler.GetUpperCaseValue()}");
 
-                // Вивід довжини
+               
                 Console.WriteLine($"Length: {handler.GetLength()}");
 
-                // 4. Автоматична заміна символів (A та a на @)
-                // Викликаємо метод двічі: для великої та малої літери
                 handler.ReplaceChar('A', '@');
                 handler.ReplaceChar('a', '@');
 
               
 
-                // 5. Вивід зміненого рядка
                 Console.WriteLine($"After replacement: {handler.GetCurrentValue()}");
 
              
