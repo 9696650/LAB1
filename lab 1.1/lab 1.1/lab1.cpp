e76a97c8d99a8115c94c8c05c586e47d05117066
Xxy8gxiydy8d8ydy8dxyixtixtixoyxy8x8ycyociycidyic8yxitxtixiyxyxyixyix <9696650@stud.kai.edu.ua>
2026-02-23 23:57:58 +0200

    Update lab1.cpp

--- lab 1.1/lab 1.1/lab1.cpp
+++ lab 1.1/lab 1.1/lab1.cpp
@@ -18,17 +18,9 @@ size_t String_Class::Get_Length() {
 }
 
 void String_Class::To_Upper() {
- 
-    for (size_t i = 0; i < value.length(); i++) {
-        value[i] = (char)toupper((unsigned char)value[i]);
-    }
-}
 
-void String_Class::Replace_Char(char oldChar, char newChar) {
-    for (size_t i = 0; i < value.length(); i++) {
-        if (value[i] == oldChar) {
-            value[i] = newChar;
-        }
-    }
+for (size_t i = 0; i < value.length(); i++) {
+    value[i] = (char)toupper((unsigned char)value[i]);
+}
 }
 
