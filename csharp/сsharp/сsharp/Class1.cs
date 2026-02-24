04d99a79f3dcf33a33356c54f2d7342e3eadc2b4
Xxy8gxiydy8d8ydy8dxyixtixtixoyxy8x8ycyociycidyic8yxitxtixiyxyxyixyix <9696650@stud.kai.edu.ua>
2026-02-24 08:36:47 +0200

    Update Class1.cs

--- csharp/сsharp/сsharp/Class1.cs
+++ csharp/сsharp/сsharp/Class1.cs
@@ -4,33 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-
 namespace csharp
 {
-    public class MyString
+    class MyString
+
     {
-       
         private string text;
 
-       
         public MyString(string input)
         {
-            text = input.ToUpper(); 
+            text = input.ToUpper();
         }
 
-        
         public string GetText()
         {
             return text;
         }
 
-        
         public int GetLength()
         {
             return text.Length;
         }
-
-       
     }
 }
